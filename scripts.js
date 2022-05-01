65b71d6684a4d6b827ab6f3c3cca7e8fba4e4902
Igor <igordecastro07@gmail.com>
2022-05-01 12:52:15 -0300

    requisito - montar blusa

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-console.log("Opaaa");
+const nome = prompt("Qual é seu nome?");
+
+function selecionaOpcao(el) {
+    const pai = el.parentNode;
+    const opcao = el.querySelector(".div-arredondada");
+    if (pai.querySelector(".selecionado") !== null) {
+        pai.querySelector(".selecionado").classList.remove("selecionado")
+        opcao.classList.add("selecionado");
+    } else {
+        opcao.classList.add("selecionado");
+    }
+    verificaSelecao();
+}
+
+function verificaSelecao() {
+    if (document.querySelector(".modelo .selecionado") !== null && document.querySelector(".gola .selecionado") !== null && document.querySelector(".tecido .selecionado") !== null && document.querySelector("link").value !== "") {
+        return true;
+    }
+    return false;
+}
